a8f10b0a82ba4a1eca8b46b0cf56961274843088
Phoebe0524 <63526084+Phoebe0524@users.noreply.github.com>
2020-06-11 10:11:53 -0400

    delete readme

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import Counter from './Components/Counter/counter';
+ 
 ReactDOM.render(
   <React.StrictMode>
     <App />
